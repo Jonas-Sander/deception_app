36d1b8a61947233097a98d16d5d576ad091f8b5e
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2021-03-07 21:42:56 +0100

    Added second textfield, button and username

--- lib/email_customization/view/email_customization_page.dart
+++ lib/email_customization/view/email_customization_page.dart
@@ -5,6 +5,7 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:deception_app/app/deception_colors.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -31,12 +32,32 @@ class CounterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 65),
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            Align(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 65),
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    'Haunt',
+                    style: GoogleFonts.raleway(
+                      textStyle: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .copyWith(fontSize: 20),
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Align(
                 alignment: Alignment.topLeft,
                 child: Text(
                   'Deine E-Mail-Adressen',
@@ -46,72 +67,76 @@ class CounterView extends StatelessWidget {
                     fontStyle: FontStyle.italic,
                     color: Colors.white,
                   ),
-                )),
-            const Divider(),
-            const SizedBox(height: 100),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Flexible(
-                      flex: 1,
-                      child: AutoSizeTextField(
-                        maxLength: 20,
-                        // maxLengthEnforced: true,
-                        minWidth: 50,
-                        fullwidth: false,
-                        textAlign: TextAlign.end,
-                        controller: TextEditingController(),
-                        inputFormatters: [
-                          // TextInputFormatter.withFunction(
-                          //     (oldValue, newValue) => EmailValidator.validate(
-                          //             '$newValue@deception.team')
-                          //         ? newValue
-                          //         : oldValue),
-                        ],
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText2!
-                            .copyWith(fontSize: 20),
-                      ),
-                    ),
-                    Flexible(
-                        flex: 2,
-                        child: Text(
-                          '@deception.team',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText2!
-                              .copyWith(fontSize: 20),
-                        )),
-                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+              const Divider(),
+              const SizedBox(height: 80),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  EmailTextField(),
+                  Opacity(opacity: 0.3, child: EmailTextField()),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Align(
+                  alignment: Alignment.centerRight,
+                  child: ElevatedButton(
+                      style: ButtonStyle(),
+                      onPressed: () {},
+                      child: Text(
+                        'AB GEHTS',
+                        style: GoogleFonts.raleway(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white,
+                        ),
+                      ))),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            key: const Key('counterView_increment_floatingActionButton'),
-            child: const Icon(Icons.add),
-            onPressed: () => context.read<CounterCubit>().increment(),
+    );
+  }
+}
+
+class EmailTextField extends StatelessWidget {
+  const EmailTextField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          flex: 1,
+          child: AutoSizeTextField(
+            maxLength: 20,
+            decoration: InputDecoration(
+                focusColor: Colors.white,
+                fillColor: Colors.white,
+                hoverColor: Colors.white),
+            minWidth: 50,
+            fullwidth: false,
+            textAlign: TextAlign.end,
+            controller: TextEditingController(),
+            style:
+                Theme.of(context).textTheme.bodyText2!.copyWith(fontSize: 20),
           ),
-          const SizedBox(height: 8),
-          FloatingActionButton(
-            key: const Key('counterView_decrement_floatingActionButton'),
-            child: const Icon(Icons.remove),
-            onPressed: () => context.read<CounterCubit>().decrement(),
-          ),
-        ],
-      ),
+        ),
+        Flexible(
+            flex: 2,
+            child: Text(
+              '@deception.team',
+              style:
+                  Theme.of(context).textTheme.bodyText2!.copyWith(fontSize: 20),
+            )),
+      ],
     );
   }
 }
